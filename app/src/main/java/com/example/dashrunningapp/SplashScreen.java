d7a4bdb033382d1439c0466ccaccb2638ec9de4e
jenirobbie <jenirobbie@gmail.com>
2020-06-03 14:15:32 +0100

    finalised positioning

--- app/src/main/java/com/example/dashrunningapp/SplashScreen.java
+++ app/src/main/java/com/example/dashrunningapp/SplashScreen.java
@@ -5,11 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+
+
 public class SplashScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        startActivity(new Intent (this,MainActivity.class));
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+
+        // Following the documentation, right after starting the activity
+        // we override the transition
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
