abd0441dabb1136bb9dfcaae82d05a3c9dfbf38e
jenirobbie <jenirobbie@gmail.com>
2020-06-03 16:38:14 +0100

    Made a start to the log in UI

--- app/src/main/java/com/example/dashrunningapp/SplashScreen.java
+++ app/src/main/java/com/example/dashrunningapp/SplashScreen.java
@@ -11,26 +11,26 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreen extends AppCompatActivity {
 
-private FirebaseAuth mAuth;
+//private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        mAuth = FirebaseAuth.getInstance();
+        //mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
 
-    @Override
+   /* @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
-    }
+    }*/
 }
 
 
