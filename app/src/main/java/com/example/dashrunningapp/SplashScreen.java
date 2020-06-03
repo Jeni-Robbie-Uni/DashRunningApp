71f86a0b7e5b60968cfca716fd5a92e7f61163f2
jenirobbie <jenirobbie@gmail.com>
2020-06-03 15:22:10 +0100

    added in firbase functionality to integrate facebook login easily

--- app/src/main/java/com/example/dashrunningapp/SplashScreen.java
+++ app/src/main/java/com/example/dashrunningapp/SplashScreen.java
@@ -5,23 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class SplashScreen extends AppCompatActivity {
 
+private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
-
         Intent intent = new Intent(this, MainActivity.class);
-
         startActivity(intent);
 
-        // Following the documentation, right after starting the activity
-        // we override the transition
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
+
+
