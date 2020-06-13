f350a3cd774be727a727e4747271060483d4059c
jenirobbie <jenirobbie@gmail.com>
2020-06-13 18:30:41 +0100

    minor changes

--- app/src/main/java/com/example/dashrunningapp/SignUp.java
+++ app/src/main/java/com/example/dashrunningapp/SignUp.java
@@ -1,21 +1,19 @@
 package com.example.dashrunningapp;
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.lang.annotation.Native;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class SignUp extends AppCompatActivity {
 
@@ -66,10 +64,16 @@ public class SignUp extends AppCompatActivity {
 
 
 
+
         });
 
 
     }
+
+
+
+    RequestQueue queue = Volley.newRequestQueue(this);
+
 
 
 
