a00f6cbb402e401bfd41436daeb03a2077d033f3
jenirobbie <jenirobbie@gmail.com>
2020-06-15 00:08:51 +0100

    Included Volley library, set up singleton request class and wrote code to change userdetails object to Json object. The Json object is then passed to api successfully.

--- app/src/main/java/com/example/dashrunningapp/SignUp.java
+++ app/src/main/java/com/example/dashrunningapp/SignUp.java
@@ -1,5 +1,7 @@
 package com.example.dashrunningapp;
 
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +11,27 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class SignUp extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +76,59 @@ public class SignUp extends AppCompatActivity {
                 Toast.makeText(SignUp.this,"Hey " +
                         current_user.getFName(), Toast.LENGTH_LONG).show();
 
+
+                String userJson="";
+                try {
+
+
+                    // convert user object to JSON
+                    userJson = new Gson().toJson(current_user);
+
+                    // print JSON string
+                    System.out.println(userJson);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+                JSONObject userJsonObject=null;
+
+                try {
+                    userJsonObject= new JSONObject(userJson);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                 Context m_context = getApplicationContext();
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = VolleyQueue.getInstance(m_context).
+                        getRequestQueue();
+                String url ="http://localhost:5000" + "/api/Users";
+
+                // Request a string response from the provided URL.
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, url, userJsonObject, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(SignUp.this,"Hey " +
+                                        response.toString(), Toast.LENGTH_LONG).show();
+
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+                                Toast.makeText(SignUp.this, error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+
+
+                // Add the request to the RequestQueue.
+                queue.add(jsonObjectRequest);
             }
 
 
@@ -72,7 +141,7 @@ public class SignUp extends AppCompatActivity {
 
 
 
-    RequestQueue queue = Volley.newRequestQueue(this);
+    //http://localhost:5000
 
 
 
