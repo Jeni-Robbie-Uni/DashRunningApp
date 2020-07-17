51ac4079920e04ff0c995f586a667c0f058febaf
Jeni Robbie <jenirobbie@gmail.com>
2020-07-17 20:35:48 +0100

    can identify own loaction- latitude and logitude

--- app/src/main/java/com/example/dashrunningapp/ui/event/EventFragment.java
+++ app/src/main/java/com/example/dashrunningapp/ui/event/EventFragment.java
@@ -1,42 +1,111 @@
 package com.example.dashrunningapp.ui.event;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.dashrunningapp.R;
+import com.example.dashrunningapp.activity.MainActivity;
 
 import static androidx.core.content.ContextCompat.getSystemService;
 
 public class EventFragment extends Fragment {
 
     private EventViewModel eventViewModel;
-
+    LocationManager locationManager;
+    String latitude, longitude;
+    private static final int REQUEST_LOCATION = 1;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         eventViewModel =
                 ViewModelProviders.of(this).get(EventViewModel.class);
+        final Activity m_activity=getActivity();
+        ActivityCompat.requestPermissions( m_activity,
+                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+
 
         View root = inflater.inflate(R.layout.fragment_event, container, false);
         final TextView textView = root.findViewById(R.id.text_event);
-        eventViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        final Context m_context = getContext();
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            OnGPS();
+        } else {
+            getLocation(textView, locationManager);
+        }
+
+
+        return root;
+
+    }
+
+
+
+
+    private void OnGPS(){
+        final Activity m_activity=getActivity();
+        final Context m_context = getContext();
+        final AlertDialog.Builder builder = new AlertDialog.Builder(m_context);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-
-
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
             }
         });
-        return root;
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+
+
+
+    private void getLocation(TextView textView, LocationManager locationManager) {
+        final Context m_context = getContext();
+        final Activity m_activity=getActivity();
+        if (ActivityCompat.checkSelfPermission(
+                m_context,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                m_context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(m_activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+                textView.setText("Your Location: " + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+            } else {
+                Toast.makeText(m_context, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
